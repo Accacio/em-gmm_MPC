62fea40ac7a29d80e63be0a3353f419989bae5b0
Accacio <raccacio@poli.ufrj.br>
2022-01-12 17:32:40 +0100

    correct variable

--- genData.m
+++ genData.m
@@ -197,7 +197,7 @@ end
 for component=1:n;
     y=lambda_tilde(component,:);
     [C_cheat(:,:,:,component),d_cheat(:,:,:,component),responsabilities_cheat(:,:,component),~, ~] = emgm_estimate(x,y,modes,emMaxIter,eps);
-    C_cheat_estimated = sort(reshape(C(:,:,:,component),size(C,1),size(C,3)))
+    C_cheat_estimated = sort(reshape(C_cheat(:,:,:,component),size(C_cheat,1),size(C_cheat,3)))
 end
 
 
