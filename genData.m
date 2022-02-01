1f360ab406a296ef33a92cace18e894522e7efac
Accacio <raccacio@poli.ufrj.br>
2022-02-01 15:23:42 +0100

    add landmarks

--- genData.m
+++ genData.m
@@ -1,7 +1,8 @@
-%% GENERATION ===
+%% === GENERATION ===
 %% Clean variables
 close all
 clear
+warning off
 %%%
 %% Define systems
 
@@ -9,7 +10,7 @@ clear
 options = optimset('Display', 'off');
 % options = [];
 
-% Number of systems
+%= Number of systems
 M=1;
 
 Cair_mean=8;
@@ -24,7 +25,7 @@ Roaia = repmat(Roaia_mean,1,M)+(-.5+rand(1,M));
 Riwia = repmat(Riwia_mean,1,M)+(-.5+rand(1,M));
 Rowoa = repmat(Rowoa_mean,1,M)+(-.5+rand(1,M));
 
-% Define systems using 3R2C
+%= Define systems using 3R2C
 for i=1:M
     csys(:,:,1,i)=model_3R2C(Roaia(i),Riwia(i),Rowoa(i),Cwalls(i),Cair(i));
 end
@@ -40,7 +41,7 @@ dsys=c2d(csys,Te);
 % Prediction horizon
 n=2;
 
-% Gains Q and R for $\sum_{j=1}^n \|v\|^2_{Q}+\|u\|^2_{R}$
+%= Gains Q and R for $\sum_{j=1}^n \|v\|^2_{Q}+\|u\|^2_{R}$
 for i=1:M
     Q(:,:,i)=eye(n*size(dsys(:,:,1,i).C,1)); % no x no
     R(:,:,i)=eye(n*size(dsys(:,:,1,i).B,2)); % nc x nc
@@ -50,7 +51,7 @@ paren = @(x, varargin) x(varargin{:}); %
                                        % Apply index in created elements
 curly = @(x, varargin) x{varargin{:}}; %
 
-% Output prediction matrices, such $\vec{Y}=\mathcal{M}\vec{x}_k+\mathcal{C}\vec{U}$
+%= Output prediction matrices, such $\vec{Y}=\mathcal{M}\vec{x}_k+\mathcal{C}\vec{U}$
 Mmat_fun=@(sys,n) ...
          cell2mat( ...
     (repmat(mat2cell(sys.C,1),1,n) .* ...
@@ -68,12 +69,12 @@ Cmat_fun=@(sys, n) ...
      repmat(mat2cell(sys.B, size(sys.B,1), size(sys.B,2)), 1, n+1)) ...
     , tril(toeplitz(1:n))+1));
 
-% H and f, such $\frac{1}{2}\vec{U}^TH\vec{U}+f^T\vec{U}$
+%= H and f, such $\frac{1}{2}\vec{U}^TH\vec{U}+f^T\vec{U}$
 H_fun=@(Cmat,Q,R) round(Cmat'*Q*Cmat+R*eye(size(Q)),10);
 f_fun=@(Cmat,Mmat,Q,xt,Wt) Cmat'*Q*(Mmat*xt-Wt);
 
 
-% Prediction matrices for the systems
+%= Prediction matrices for the systems
 for i=1:M
     Mmat(:,:,i)=Mmat_fun(dsys(:,:,1,i),n);
     Cmat(:,:,i)=Cmat_fun(dsys(:,:,1,i),n);
@@ -96,7 +97,7 @@ umax(1:2)=inf;
 %%%
 %% Get Lambdas
 
-% Generate n-dimensional rectangular grid for combinations
+%= Generate n-dimensional rectangular grid for combinations
 % see https://accacio.gitlab.io/blog/matlab_combinations/
 % values=0:.05:4;
 values=-10:1:10;
@@ -157,14 +158,19 @@ end
 % u=ones(1,n);
 % [y,t,x]=lsim(dsys(:,:,1,2),[u(:,1) 0],0:Te:Te); % Update systems
 %%%
-%% Cheating influence on lambda
+%= Cheating influence on lambda
 % let's suppose $\tilde{\vec{\lambda}}=T\vec{\lambda}$
 
-T = ceil(rand([size(lambda,1) size(lambda,1) M])*10);
+T = (10*rand([size(lambda,1) size(lambda,1) M]));
+% T = 20*eye(n)
+% T = diag(fix(20*rand(1,n)));
+T = (T+T')/2;
+% T=T+2*eye(size(lambda,1))
 for i=1:M
-    lambda_tilde(:,:,i) = T(:,:,i)*lambda(:,:,i);
+    lambda_tilde(:,:,i) = T*lambda(:,:,i);
 end
 
+if(size(theta,1)==2)
 for i=1:M
     figure
     for j=1:size(lambda,1)
@@ -177,31 +183,71 @@ for i=1:M
         ylabel('$\theta_2$','interpreter','latex')
     end
 end
+end
 
-
-%% ESTIMATION ===
+%% === ESTIMATION ===
 PI=pi;
 emMaxIter=500;
 modes=n^2;
-maxErr=1e-4;
+maxErr=1e-8;
 x=theta;
 
-% estimate normal
+%= Estimate normal
 for component=1:n;
     y=lambda(component,:);
-    [C(:,:,:,component),d(:,:,:,component),responsabilities(:,:,component),~, ~] = emgm_estimate(x,y,modes,emMaxIter,eps);
-    C_estimated(:,:,component) = sort(reshape(C(:,:,:,component),size(C,1),size(C,3)));
+    [C(:,:,:,component),d(:,:,:,component),responsabilities(:,:,component),~, ~] = emgm_estimate (x,y,[],[],modes,emMaxIter,maxErr);
+    C_estimated(:,:,component) = -reshape(C(:,:,:,component),size(C,1),size(C,3));
+    d_estimated(:,:,component) = -reshape(d(:,:,:,component),size(d,1),size(d,3));
 end
+return
+for component=1:n
+    index_of_zero=find(sum(theta==zeros(size(theta)))==n);
+    % index_of_zero=1;
+    [~,z_hat_zero]=max(responsabilities(:,index_of_zero,component));
+    H_est(component,:,i)=C_estimated(:,z_hat_zero,component)';
+    f_est(component,:,i)=d_estimated(:,z_hat_zero,component);
+end
+H_est(:,:,i)
+H(:,:,1)
+f_est(:,:,i)
+f(:,:,1)
 
-% estimate cheating
+%= Estimate cheating
 for component=1:n;
     y=lambda_tilde(component,:);
-    [C_cheat(:,:,:,component),d_cheat(:,:,:,component),responsabilities_cheat(:,:,component),~, ~] = emgm_estimate(x,y,modes,emMaxIter,eps);
-    C_cheat_estimated(:,:,component) = sort(reshape(C_cheat(:,:,:,component),size(C_cheat,1),size(C_cheat,3)));
+
+    % Cinit= reshape(repmat(paren(T*H_est,1,':')',1,modes),2,1,modes)+rand(2,1,modes);
+    % dinit = reshape(repmat(paren(T*f_est,1,':')',1,modes),1,1,modes)+rand(1,1,modes);
+    % Cinit= reshape(repmat(paren(T*H_est,1,':')',1,modes),2,1,modes);
+    % dinit = reshape(repmat(paren(T*f_est,1,':')',1,modes),1,1,modes);
+
+    % Cinit=C(:,:,:,component);
+    % dinit=d(:,:,:,component);
+    % [C_cheat(:,:,:,component),d_cheat(:,:,:,component),responsabilities_cheat(:,:,component),~, ~] = emgm_estimate (x,y,Cinit,dinit,modes,emMaxIter,maxErr);
+    [C_cheat(:,:,:,component),d_cheat(:,:,:,component),responsabilities_cheat(:,:,component),~, ~] = emgm_estimate (x,y,[],[],modes,emMaxIter,maxErr);
+    C_cheat_estimated(:,:,component) = -reshape(C_cheat(:,:,:,component),size(C_cheat,1),size(C_cheat,3));
+    d_cheat_estimated(:,:,component) = -reshape(d_cheat(:,:,:,component),size(d_cheat,1),size(C_cheat,3));
+end
+% TODO(accacio): should not necessarily be zero, can be any theta < -P\s
+for component=1:n
+    index_of_zero=find(sum(theta==zeros(size(theta)))==n);
+    % index_of_zero=1;
+    [~,z_hat_zero]=max(responsabilities(:,index_of_zero,component));
+    H_cheat_est(component,:,i)=C_cheat_estimated(:,z_hat_zero,component)';
+    f_cheat_est(component,:,i)=d_cheat_estimated(:,z_hat_zero,component);
 end
 
-C_estimated
-C_cheat_estimated
+H_cheat_est(:,:,i)
+T*H(:,:,1)
+f_cheat_est(:,:,i)
+T*f(:,:,1)
+
+
+% C_estimated
+% C_cheat_estimated
+
+invT_est=H*inv(H_cheat_est)
+invT=inv(T)
 
 %%
 rgb=@(x,y,z) [x, y,z]/255;
