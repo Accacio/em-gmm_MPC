ffa24ae062bfdad633568e6fd6b0a0f84fd6f4f1
Accacio <raccacio@poli.ufrj.br>
2022-02-01 15:33:27 +0100

    marks

--- calculate_responsabilities.m
+++ calculate_responsabilities.m
@@ -3,12 +3,15 @@ function responsabilities = calculate_responsabilities(x, y, C, d, Sigma, pi)
 
   M=size(d,3);
   N=size(x,3);
-  respNum=zeros(size(x,2),M);
+  respNum =zeros(size(x,2),M);
+  %= calculate numerator for each mode
   for i=1:M
     w=pi(i)*sqrt(Sigma(:,:,i));
     lktemp=diag(y-[C(:,:,i)'*x+d(:,:,i)]);
     SigmaInvMat=kron(eye(N),Sigma(:,:,i)\1);
     lk=diag(-1/2*lktemp*SigmaInvMat*lktemp');
+
+    %= logsumexp trick see: https://www.cs.bgu.ac.il/~cv202/wiki.files/logsumexp_trick.pdf
     a=max(lk)+log(w);
     likelihood_mod=exp(lk+log(w)-a);
     % likelihood=mvnpdf(y',(C(:,i)'*x+d(:,i))',Sigma(:,:,i));
