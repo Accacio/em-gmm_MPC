99eceb79f73fcd10c25c2cd94fc2653f5c0024f4
Accacio <raccacio@poli.ufrj.br>
2022-02-01 15:24:16 +0100

    initializing parameters

--- emgm_estimate.m
+++ emgm_estimate.m
@@ -1,28 +1,32 @@
-function [C,d,responsabilities,pi,Sigma] = emgm_estimate(x,y,modes,emMaxIter,maxErr)
+function [C,d,responsabilities,pi,Sigma] = emgm_estimate(x,y,m,n,modes,emMaxIter,maxErr)
 % EMGM_ESTIMATE -
 
     pi=repmat(1/modes,1,modes);
     OldclusterSize=zeros(1,modes);
 
+    if(isempty(m)||isempty(n))
+        indexpts=randi([1 size(x,2)-1],1,modes);
+        dx=max(x(:,2:end)-x(:,1:end-1),[],2);
+        dx=num2cell(dx,2);
 
-    indexpts=randi([1 size(x,2)-1],1,modes);
-    dx=max(x(:,2:end)-x(:,1:end-1),[],2);
-    dx=num2cell(dx,2);
+        y_square=reshape(y,sqrt(size(y,2)),sqrt(size(y,2)));
+        [V{1:2}]=gradient(y_square,dx{:});
+        grad=cell2mat(cellfun(@(x) reshape(x,[],1),V,'UniformOutput',0))';
+        C=grad(:,indexpts);
+        x_indexed=x(:,indexpts);
+        y_indexed=y(:,indexpts);
+        d=y_indexed-sum(C.*x_indexed);
+        C=reshape(C,2,1,modes);
+        d=reshape(d,1,1,modes);
+        % C=5*rand(size(x,1),1,modes);
+        % d=5*rand(1,1,modes);
+    else
+        C=m;
+        d=n;
+    end
 
-    y_square=reshape(y,sqrt(size(y,2)),sqrt(size(y,2)));
-    [V{1:2}]=gradient(y_square,dx{:});
-    grad=cell2mat(cellfun(@(x) reshape(x,[],1),V,'UniformOutput',0))';
-    C=grad(:,indexpts)
-    x_indexed=x(:,indexpts);
-    y_indexed=y(:,indexpts);
-    d=y_indexed-sum(C.*x_indexed);
-    C=reshape(C,2,1,modes);
-    d=reshape(d,1,1,modes);
 
-    % C=5*rand(size(x,1),1,modes);
-    % d=5*rand(1,1,modes);
-
-    eps=100;
+    eps=1000;
     Sigma(:,:,1:modes)=repmat(eps*eye(1),1,1,modes);
 
     for emInd=1:emMaxIter
